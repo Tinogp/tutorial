95bcfc070a907a65ad89957330e30c7627bf98c7
tinog <tinog@Portatil_Tino>
2025-09-24 11:06:30 +0200

    3

--- src/tutorial/tutorial.java
+++ src/tutorial/tutorial.java
@@ -5,6 +5,7 @@ public class tutorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("segundo commit");
+		System.out.println("tercer commit");
 	}
 
 }
