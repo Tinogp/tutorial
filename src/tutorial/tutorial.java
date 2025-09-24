e9843717260becd4f7529f580231254741db918d
tinog <tinog@Portatil_Tino>
2025-09-24 11:31:01 +0200

    6

--- src/tutorial/tutorial.java
+++ src/tutorial/tutorial.java
@@ -5,6 +5,7 @@ public class tutorial {
 	public static void main(String[] args) {
 		System.out.println("segundo commit");
 		System.out.println("tercer commit");
+		System.out.println("sexto commit");
 	}
 
 }
